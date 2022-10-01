47ed1b9dd7c64e8524d9c1c89a3cbb5b30fdb7a1
Daniel-Zarsky <xzarsk04@stud.fit.vutbr.cz>
2022-10-01 19:24:47 +0200

    libraries included

--- scanner.c
+++ scanner.c
@@ -3,8 +3,36 @@
  *
  * @Lexical analysis
  *
+ * @authors
  */
 
+#include <ctype.h> // includes functions for determining type of loaded data
+#include <stdio.h>
+#include <buffer.h> // used for storing the identifier of token
+#include <scanner.h> // includes prototypes and list of keywords, states and types of tokens and structure of token itself
+#include <error.h>
+#include <stdlib.h> // used for string processing
+
+
+ token get_next_token(){
+
+    while(true){
+
+        //load char from source file
+
+        switch(){
+         // instructions for each state
+         //lecture notes:
+         // reduce states with direct transfer to other states(without condition)
+         // only deterministic states
+         // check unreachable states
 
 
 
+        }
+
+
+    }
+
+
+}
---
+++ scanner.h
@@ -0,0 +1,12 @@
+/**
+ * Projekt: IFJ 2022
+ *
+ * @Lexical analysis library
+ *
+ * @authors
+ *
+ */
+
+#include <stdio.h>
+#include <buffer.h>
+
