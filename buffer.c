e9e7e0997e0f0d6e0326917c956f4aa2069eae9c
Daniel-Zarsky <xzarsk04@stud.fit.vutbr.cz>
2022-10-02 10:42:34 +0200

    errors added

---
+++ buffer.c
@@ -0,0 +1,9 @@
+/**
+ * Projekt: IFJ 2022
+ *
+ * @Support of lexical analysis
+ * @providing dynammic string buffer data type
+ * @plus basic functions of it
+ *
+ * @authors
+ */
--- scanner.h
+++ scanner.h
@@ -10,3 +10,43 @@
 #include <stdio.h>
 #include <buffer.h>
 
+
+enum keyword{
+ //else, float, function, if, int, null, return, string,
+    //void, while
+};
+
+enum token_type{
+    //end of line/file
+    //keyword
+  // logical operator
+  //arithmetic operator
+  // data type identifier
+  // (),;
+};
+
+union token_attribute{
+//buffer - dynamic string
+ int integer;
+  float decimal; // see instructions in elearning
+};
+
+typedef struct token{
+//type
+//attribute
+}token;
+
+int load_buffer();
+int free_buffer();
+//initialize buffer and unload - dynamic string
+token get_next_token();
+int process_string(); //identify keyword or make new token name
+int process_int(); //
+int process_float();//
+//main function with while
+//prototypes of processing functions
+// get source file
+
+enum scanner_state{
+
+};
