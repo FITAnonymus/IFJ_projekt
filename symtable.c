d964dc485de24efcc2714cacae68b5101d3ddc8b
Martin Tomašovič <xtomas36@vutbr.cz>
2022-10-30 22:32:48 +0100

    Feat improved size of hash table

--- symtable.c
+++ symtable.c
@@ -153,6 +153,143 @@ char* search(Hash_table_ptr *p_table, char* key, char* type) {
     return NULL;
 }
 
+// Functions for function hash-table
+
+int create_fitem(char* key, char* value, char* type, ItemPtr* new_item) {
+
+    ItemPtr p_item = (ItemPtr) malloc (sizeof(Item));
+    if(p_item != NULL){
+        p_item->key = (char*) malloc (strlen(key) + 1);
+        p_item->value = (char*) malloc (strlen(value) + 1);
+        p_item->type = (char*) malloc (strlen(type) + 1);
+
+        if((p_item->key == NULL) || (p_item->value == NULL || p_item->type == NULL)){
+            return ERR_INTERNAL;
+        }
+    
+        strcpy(p_item->key, key);
+        strcpy(p_item->value, value);
+        strcpy(p_item->type, type);
+        p_item->next = NULL;
+    } else {
+        return ERR_INTERNAL;
+    }
+    *new_item = p_item;
+    return 0;
+}
+
+int create_table(int size, Hash_table_ptr *hashTPtr) {
+
+    Hash_table_ptr h_table = (Hash_table_ptr) malloc (sizeof(Hash_table));
+    if(h_table != NULL){
+        h_table->size = size;
+        h_table->items = (ItemPtr*) calloc (h_table->size, sizeof(ItemPtr));
+        if(h_table->items != NULL){
+            for (int i = 0; i < h_table->size; i++){
+                h_table->items[i] = NULL;
+            }
+            *hashTPtr = h_table;
+        } else {
+            return ERR_INTERNAL;
+        }
+    } else {
+        return ERR_INTERNAL;
+    }
+    return 0;
+}
+
+void free_items(ItemPtr* p_item) {
+
+    ItemPtr item;
+    item = *p_item;
+    ItemPtr help_pointer_item;
+
+    while(item != NULL){
+        
+        help_pointer_item = item->next;
+
+        // free all parts of item
+        free(item->key);
+        free(item->value);
+        free(item->type);
+
+        // erase pointer to the item itself
+        free(item);
+
+        item = help_pointer_item;
+    }
+}
+
+void free_table(Hash_table_ptr p_table) {
+    for (int i = 0; i < p_table->size; i++) {
+        ItemPtr item = p_table->items[i];
+        if (item != NULL)
+            free_items(&item);
+    }
+
+    free(p_table->items);
+    free(p_table);
+}
+
+int insert(Hash_table_ptr *p_table, char* key, char* value, char* type) {
+    ItemPtr p_item = NULL;
+    create_item(key, value, type, &p_item);
+    if(p_item != NULL){
+        // Get index
+        unsigned long index = hash(key);
+
+        ItemPtr current_item = (*p_table)->items[index];
+    
+        if (current_item == NULL) {
+            // Key does not exist
+            // Create hash table item
+            (*p_table)->items[index] = p_item;
+        } else {
+            // Go through all items with same hash and add new item to the end
+            while(current_item->next != NULL){
+                current_item =  current_item->next;
+            }
+            current_item->next = p_item;
+            //current_item->next = NULL;
+        }
+    } else {
+        return ERR_INTERNAL;
+    }
+    return 0;
+}
+
+char* search(Hash_table_ptr *p_table, char* key, char* type) {
+    // Searches the key in the hashtable, returns NULL if it doesn't exist
+    int index = hash(key);
+    ItemPtr p_item = (*p_table)->items[index];
+
+    // Ensure that we move to a non NULL item
+    short continue_search = 0;
+    if (p_item != NULL && (strcmp(p_item->type, type) != 0)){
+        continue_search = 1;
+    }
+    while(continue_search){
+        if((strcmp(p_item->key, key) == 0) && (strcmp(p_item->type, type) == 0)){
+            continue_search = 0;
+        }
+        else {
+            p_item = p_item->next;
+            if(p_item == NULL){
+                continue_search = 0;
+            }
+        }
+    }
+    // if item pointer isn't null, it means we found our key
+    if (p_item != NULL){
+        return p_item->value;
+
+        
+    }
+    // there is no such key
+    return NULL;
+}
+
+
 // for testing
 void print_search(Hash_table_ptr* table, char* key, char* type) {
     char* val;
--- symtable.h
+++ symtable.h
@@ -1,5 +1,5 @@
-//length of table
-unsigned const int LENGTH = 1024;
+//length of table inspired by https://planetmath.org/goodhashtableprimes
+unsigned const int LENGTH = 1543;
 
 //Definition of Item of table 
 typedef struct item {
@@ -18,4 +18,25 @@ typedef struct hash_table {
     int size;
 }Hash_table;
 
+// Pointer to Hash_table
 typedef Hash_table *Hash_table_ptr;
+
+//Definition of FunctionItem of table 
+typedef struct fitem {
+    Item params;
+    Item ret_val;
+    int num_of_items;
+    struct fitem *next;
+} fItem;
+
+// Pointer to fItem
+typedef fItem *fItemPtr;
+
+// Definition of the hash table for functions
+typedef struct fhash_table {
+    fItem** fitems;
+    int size;
+}fHash_table;
+
+// Pointer to fHash_table
+typedef fHash_table *fHash_table_ptr;
