04d5056b204b8974bbcf174d31dfeb2cc8a1486e
samuelsimun <samuelsimun@gmail.com>
2022-11-18 23:48:55 +0100

    Added documentation for each function

--- syntactic.c
+++ syntactic.c
@@ -26,7 +26,14 @@
 #define TRUE 1
 
 
-void Destroy_data(Syntactic_data_ptr to_delete){
+/**
+ * @brief Function for deallocating all sources
+ * the function handles end of program - free all allocated memory
+ *
+ * @param Syntactic_data_ptr -  Data set which will be freed
+ * @return void
+ */
+void Destroy_data(Syntactic_data_ptr to_delete) {
     if (to_delete == NULL) {
         return;
     }
@@ -35,16 +42,32 @@ void Destroy_data(Syntactic_data_ptr to_delete){
     free_table(to_delete->local_var);
     free_table(to_delete->function_var);
     free(to_delete);
+}
 
+
+/**
+ * @brief Function handles unexpected end of parser
+ * the function handles program error -> calling Destroy data and exiting with
+ * with error code
+ *
+ * @param int
+ * @param Syntactic_data_ptr
+ * @return void
+ */
 void Program_Error(int error, Syntactic_data_ptr data){
     Destroy_data(data);
     exit(error);
 }
 
 
-
+/**
+ * @brief Function of initializing data sources
+ * Function allocates data sources and sets to default values
+ *
+ * @return Syntactic_data_ptr
+ */
 Syntactic_data_ptr Init_data(){
-    Syntactic_data_ptr *data_ptr = malloc(sizeof(struct Syntactic_data));
+    Syntactic_data_ptr data_ptr = malloc(sizeof(struct Syntactic_data));
     if (data_ptr == NULL){
         Program_Error(ERR_INTERNAL);
     }
@@ -60,15 +83,28 @@ Syntactic_data_ptr Init_data(){
     return data_ptr;
 }
 
+
+/**
+ * @brief Function get new token from file
+ * Function get new token from file and return it for next processing
+ *
+ * @return token_struct token
+ */
 token_struct Get_token(){
     token_struct token;
     get_next_token(&token);
     return token;
 }
 
-}
 
-
+/**
+ * @brief Function handles start of command with keyword function
+ * Function handles token Function by calling function from gramatic_rules.c
+ *
+ * @see gramatic_rules.c
+ * @param Syntactic_data_ptr
+ * @return void
+ */
 int Handle_function_dec(Syntactic_data_ptr data){
     /// Create local sym_table for function
     create_table(1543, data->local_var);
@@ -86,6 +122,15 @@ int Handle_function_dec(Syntactic_data_ptr data){
 
 }
 
+
+/**
+ * @brief Function handles start of command with keyword if
+ * Function handles token Function by calling function from gramatic_rules.c
+ *
+ * @see gramatic_rules.c
+ * @param Syntactic_data_ptr
+ * @return void
+ */
 int Handle_if(Syntactic_data_ptr data){
     /// Create local sym_table for condition
     create_table(1543, data->local_var);
@@ -101,6 +146,15 @@ int Handle_if(Syntactic_data_ptr data){
     return SYNTAX_OK;
 }
 
+
+/**
+ * @brief Function handles start of command with keyword while
+ * Function handles token Function by calling function from gramatic_rules.c
+ *
+ * @see gramatic_rules.c
+ * @param Syntactic_data_ptr
+ * @return void
+ */
 int Handle_while(Syntactic_data_ptr data){
     /// Create local sym_table for while
     create_table(1543, data->local_var);
@@ -116,38 +170,50 @@ int Handle_while(Syntactic_data_ptr data){
     return SYNTAX_OK;
 }
 
+
+/**
+ * @brief Function handles start of command with keyword Int
+ * Function handles token Function by calling function from gramatic_rules.c
+ *
+ * @see gramatic_rules.c
+ * @param Syntactic_data_ptr
+ * @return void
+ * @TODO Write
+ */
 int Handle_int(Syntactic_data data){
 
 }
 
+/**
+ * @brief Function handles start of command with keyword Float
+ * Function handles token Function by calling function from gramatic_rules.c
+ *
+ * @see gramatic_rules.c
+ * @param Syntactic_data_ptr
+ * @return void
+ * @TODO Write
+ */
 int Handle_float(Syntactic_data data){
 
 }
 
+/**
+ * @brief Function handles start of command with keyword String
+ * Function handles token Function by calling function from gramatic_rules.c
+ *
+ * @see gramatic_rules.c
+ * @param Syntactic_data_ptr
+ * @return void
+ * @TODO Write
+ */
 int Handle_string(Syntactic_data data){
 
 }
 
 
-
-int main(){
-    token_struct token = get_next_token();
-
-    if (validate_program(token)){
-        Program_Error(ERR_SYNTAX, data);;
-    }
-
-    Syntactic_data_ptr *data = Init_data();
-    create_table(1543, data->main_var);
-    create_table(1543, data->function_var);
-    parser();
-
-    return 0;
-}
-
 int parser(Syntactic_data_ptr data){
 
-    token_struct token = get_next_token();
+    token_struct token = Get_token();
 
     while(token.type != TYPE_PROLOG_END || token.type != TYPE_EOF) {
         switch (token.type) {
@@ -203,7 +269,25 @@ int parser(Syntactic_data_ptr data){
                 Program_Error(ERR_SYNTAX, data);
         }
 
-        token = get_next_token();
+        token = Get_token();
     }
 
 }
+
+
+int main(){
+    token_struct token = get_next_token();
+
+    if (validate_program(token)){
+        Program_Error(ERR_SYNTAX, data);;
+    }
+
+    Syntactic_data_ptr *data = Init_data();
+    create_table(1543, data->main_var);
+    create_table(1543, data->function_var);
+    parser();
+
+    return 0;
+}
+
+
