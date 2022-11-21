616bdc291758d18f40922f478e2b6164298150f4
gorge <jiso3@seznam.cz>
2022-11-21 16:01:01 +0100

    functions for syntactic analyse, more functions

---
+++ expression.c
@@ -0,0 +1,10 @@
+//
+// Created by gorge on 21.11.22.
+//
+
+#include "expression.h"
+#include "syntactic.h"
+
+int check_expression(Token_struct token, Syntactic_data_ptr data){
+    return 0;
+}
---
+++ expression.h
@@ -0,0 +1,16 @@
+//
+// Created by Peaky Blinders
+//
+
+#ifndef IFJ_PROJEKT_EXPRESSION_H
+#define IFJ_PROJEKT_EXPRESSION_H
+
+#endif //IFJ_PROJEKT_EXPRESSION_H
+
+
+typedef enum{
+    E_OPEN,		//!< push (<)
+    E_CLOSE,	//!< reduce (>)
+    E_EQUAL,	//!< equal (=)
+    E_EMPTY		//!< nondefined (_)
+} stack_do;
