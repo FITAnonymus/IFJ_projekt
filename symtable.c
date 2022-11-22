467e721ddc4740ec5b052ee7c57247480fb0f348
Martin Tomašovič <xtomas36@vutbr.cz>
2022-11-22 22:39:17 +0100

    semantics condition checking start if and while processing fix symtable documentation

--- symtable.c
+++ symtable.c
@@ -499,7 +499,6 @@ int pinsert_by_type(PHash_table_ptr *p_table, char* key, char* value, int type, 
  *
  * @param p_table Pointer to the table to which we want to add new item.
  * @param key Array of chars - String. (name of function) Used for finding item in hashtable.
- * @param type Array of chars - String. Used for checking type of given entity.
  * @return Returns first pitem of given function
  */
 PItemPtr name_psearch(PHash_table_ptr *p_table, char* key) {
