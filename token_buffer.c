ce29f12437dbb062c2836d6e974e2eb91182106a
Daniel-Zarsky <xzarsk04@stud.fit.vutbr.cz>
2022-11-24 18:01:54 +0100

    token_buffer fixed

--- token_buffer.c
+++ token_buffer.c
@@ -17,29 +17,28 @@
 int init_token_buffer(Token_buffer *tok_buf){
     tok_buf->length = 0;
     tok_buf->size = sizeof(Token_struct);  ///change
-    tok_buf->token = (Token_struct *) malloc(sizeof(Token_struct));
+    tok_buf->token = (Token_struct *) malloc(sizeof(Token_struct)); ///change
+    //tok_buf->token[0] = NULL;
     return 0;
+
 }
 
 int add_token_buffer(Token_struct token,Token_buffer *tok_buf){
-    if( tok_buf != NULL){
-        if(tok_buf->length + 1 >= tok_buf->size) {
+    if(tok_buf->size +1 >= tok_buf->size) {
 
-            unsigned int size_to_alloc = (tok_buf->size + sizeof(Token_struct));///one more cell
+        unsigned int size_to_alloc = (tok_buf->size + sizeof(Token_struct));///one more cell
 
-            tok_buf->token = (Token_struct *) realloc(tok_buf->token, size_to_alloc); /// try to alloc the cell
+        tok_buf->token = (Token_struct *) realloc((Token_struct * )tok_buf->token, size_to_alloc); /// try to alloc the cell
 
-            if (tok_buf->token == NULL) { return ERR_INTERNAL; }/// check if allocation was successful
+        if (tok_buf->token == NULL) { return ERR_INTERNAL; }/// check if allocation was successful
 
-            tok_buf->size = size_to_alloc;///new size of buffer
-        }
+        tok_buf->size = size_to_alloc;///new size of buffer
+    }
 
-        tok_buf->token[tok_buf->length] = token; ///new token added to token array
-        tok_buf->length++;
-        return 0;
-
+    tok_buf->token[tok_buf->length] = token; ///new token added to token array
+    tok_buf->length++;
+    return 0;
 }
-
 void free_token_buffer(Token_buffer *tok_buf) {
     free(tok_buf->token);
     tok_buf = NULL;
