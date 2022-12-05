e34ef20924de7c6d598bee85d7fc347cf5abe450
samuelsimun <samuelsimun@gmail.com>
2022-12-05 14:43:10 +0100

    Added semantics

--- syntactic.c
+++ syntactic.c
@@ -55,7 +55,7 @@ void Destroy_data(Syntactic_data_ptr to_delete) {
  * @return void
  */
 void Program_Error(int error, Syntactic_data_ptr data){
-    printf("Program ukončený s hodnotou %d",error);
+    //printf("Program ukončený s hodnotou %d",error);
     Destroy_data(data);
     exit(error);
 }
@@ -562,6 +562,9 @@ int parser(Syntactic_data_ptr data){
         token = Get_token(data);
 
     }
+
+    if (semantics_main(data))
+        Program_Error(data->error_status, data);
 
     return SYNTAX_OK;
 }
